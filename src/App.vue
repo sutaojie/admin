e240f2426f757d65fcfff265c1dbe8c2e165e90c
sutaojie <sutaojie@gmail.com>
2018-08-30 20:20:16 +0800

    创建用户界面

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <user-pane></user-pane>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+    import  UserPane from './components/UserPane'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+      UserPane
   }
 }
 </script>
 
 <style>
+    *{padding: 0; margin: 0;}
+    *::before,*::after,*{
+        box-sizing: border-box;
+    }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/UserPane.vue
@@ -0,0 +1,43 @@
+<template>
+    <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+            <el-header>Header</el-header>
+            <el-main>Main</el-main>
+            <el-footer>Footer</el-footer>
+        </el-container>
+    </el-container>
+</template>
+
+<script>
+    export default {
+        name: "UserPane"
+    }
+</script>
+
+<style lang='scss' scoped>
+    .el-container{
+        height: 100vh;
+    }
+    .el-header, .el-footer {
+        background-color: #B3C0D1;
+        color: #333;
+        text-align: center;
+        line-height: 60px;
+    }
+
+    .el-aside {
+        background-color: #D3DCE6;
+        color: #333;
+        text-align: center;
+        line-height: 200px;
+    }
+
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        text-align: center;
+        line-height: 160px;
+    }
+
+</style>
